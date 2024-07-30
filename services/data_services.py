84ecee06d0c68d0863f5a4fd1473dcaf1d644da7
msedeno23 <msedeno.blikon@gmail.com>
2024-07-30 15:04:52 -0600

    delete file

--- services/data_services.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-def get_data(data_type):
-    file_path = os.path.join(os.getcwd(), 'data', f'{data_type}.json')
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    return data
